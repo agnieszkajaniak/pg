3190a17542c0daedfbc1482e09eea64abd0188e3
Aga J <agnieszka.janiakk@gmail.com>
2018-03-25 19:58:30 +0200

    statistics

--- zadanie1.py
+++ zadanie1.py
@@ -1,4 +1,6 @@
 import csv
+import numpy as np
+
 data = {}
 with open('dane.txt','rb') as csvfile:
     reader = csv.reader(csvfile, delimiter='\t')
@@ -14,4 +16,15 @@ def string_to_number(text):
 
 for column_name in ['Powierzchnia', 'Ludnosc','Zaludnienie']:
     data[column_name] = map(string_to_number, data[column_name])
+
+
+def stats(array):
+    tab = np.array([i for i in array if i != 'NA'])
+    return { 'mean': np.mean(tab), 'median': np.median(tab), 'std': np.std(tab), 
+             'min' : np.amin(tab), 'max' : np.amax(tab) }
+
+statistics = {}
+for column_name in ['Powierzchnia', 'Ludnosc','Zaludnienie']:
+    statistics[column_name] = stats(data[column_name])
+
 
