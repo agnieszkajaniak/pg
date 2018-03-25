aa0e0c16cca340234fe85464b9a86ebf1fd6f39b
Aga J <agnieszka.janiakk@gmail.com>
2018-03-25 19:13:47 +0200

    replace , with .

--- zadanie1.py
+++ zadanie1.py
@@ -5,3 +5,13 @@ with open('dane.txt','rb') as csvfile:
     for column in zip(*reader):
         data[column[0]] = column[1:]
 
+
+def string_to_number(text):
+    try:
+        return float(text.replace(',','.'))
+    except:
+        return 'NA'
+
+for column_name in ['Powierzchnia', 'Ludnosc','Zaludnienie']:
+    data[column_name] = map(string_to_number, data[column_name])
+
